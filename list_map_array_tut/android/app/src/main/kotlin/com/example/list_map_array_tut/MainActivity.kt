b4e193cc0ccf0619c26f627af48ac30890a6074d
Sudipta Gupta <130888739+sudiptagupta17@users.noreply.github.com>
2023-09-28 09:35:33 +0530

    friends api

---
+++ list_map_array_tut/android/app/src/main/kotlin/com/example/list_map_array_tut/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.list_map_array_tut
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ salestop/android/app/src/main/kotlin/com/example/salestop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.salestop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
